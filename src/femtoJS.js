5f6ebe4a713b9b1bd3e2e198c4fe14e554ed1b60
Logan Darklock <logandarklock@gmail.com>
2019-03-08 09:01:46 -0800

    Huge update, docs, the thing itself, and stuff

--- src/femtoJS.js
+++ src/femtoJS.js
@@ -1,41 +1,100 @@
 (() => {
 	let argToElements = function(src) {
-		let elems = []
+		if (typeof src === 'string') {
+			const tagName = /^<(\w+)>$/.exec(src)
 
-		if (typeof src == 'string') {
-			elems = [...document.querySelectorAll(src)]
+			if (tagName !== null) {
+				return [document.createElement(tagName[1])]
+			} else {
+				return [...document.querySelectorAll(src)]
+			}
 		} else if (src instanceof HTMLElement) {
-			elems.push(src)
+			return [src]
 		} else if (Array.isArray(src)) {
+			const elems = []
+
 			src.forEach(i => elems.push(...argToElements(i)))
+
+			return elems
+		} else if ('isFemtoJS' in src) {
+			return src.sel()
 		}
 
-		return elems
+		throw TypeError('Expected string | HTMLElement | Array | femtoJS,' +
+		                ' got ' + typeof src)
 	}
 
-	window.$ = function(src) {
+	const $ = function(...src) {
 		let sel = argToElements(src)
+		let iter = sel.forEach.bind(sel)
+
+		let insertToAdjacent =
+			(s) => function(e) {
+				iter((j, i) => i === 0
+				               ? e instanceof HTMLElement
+				                 ? e.insertAdjacentElement(s, j)
+				                 : e.sel()[0].insertAdjacentElement(s, j)
+				               : sel[0].insertAdjacentElement('afterend', j))
+
+				return this
+			}
+
+		let insertAdjacent =
+			(s) => function(sOrE) {
+				if (typeof sOrE !== 'string') {
+					if (sOrE instanceof HTMLElement) {
+						sel[0].insertAdjacentElement(s, sOrE)
+					} else if ('isFemtoJS' in sOrE) {
+						const osel = sOrE.sel()
+
+						sel[0].insertAdjacentElement(s, osel[0])
+
+						for (let i = 1; i < osel.length; i++) {
+							osel[0].insertAdjacentElement('afterend', osel[i])
+						}
+					}
+				} else {
+					sel[0].insertAdjacentHTML(s, sOrE)
+				}
+
+				return this
+			}
 
 		return {
-			on:    function(type, fn) { sel.forEach(i => i.addEventListener(type, fn, false));    return this },
-			css:          function(v) { sel.forEach(i => i.style.cssText += v);                   return this },
-			html:         function(v) { sel.forEach(i => i.innerHTML = v);                        return this },
-			insertBefore: function(v) { sel.forEach(i => i.insertAdjacentHTML('beforeBegin', v)); return this },
-			insertAfter:  function(v) { sel.forEach(i => i.insertAdjacentHTML('afterEnd', v));    return this },
-			insertFirst:  function(v) { sel.forEach(i => i.insertAdjacentHTML('afterBegin', v));  return this },
-			insertLast:   function(v) { sel.forEach(i => i.insertAdjacentHTML('beforeEnd', v));   return this },
-			text:         function(v) { sel.forEach(i => i.innerText = v);                        return this },
-			addClass:     function(v) { sel.forEach(i => i.classList.add(v));                     return this },
-			toggleClass:  function(v) { sel.forEach(i => i.classList.toggle(v));                  return this },
-			removeClass:  function(v) { sel.forEach(i => i.classList.remove(v));                  return this },
-			empty:         function() { sel.forEach(i => i.innerHTML = '');                       return this },
-			attr:      function(a, v) { sel.forEach(i => i.setAttribute(a, v));                   return this },
-			removeAttr:   function(v) { sel.forEach(i => i.removeAttribute(v));                   return this },
-			parent:        function() { sel.forEach((v, i) => { sel[i] = v.parentNode });         return this },
+			on:   function(type, fn) { iter(i => i.addEventListener(type, fn, false)); return this },
+			css:         function(s) { iter(i => i.style.cssText += s);                return this },
+			html:        function(h) { iter(i => i.innerHTML = h);                     return this },
+			text:        function(t) { iter(i => i.innerText = t);                     return this },
+			addClass:    function(t) { iter(i => i.classList.add(t));                  return this },
+			toggleClass: function(t) { iter(i => i.classList.toggle(t));               return this },
+			removeClass: function(t) { iter(i => i.classList.remove(t));               return this },
+			empty:        function() { iter(i => i.innerHTML = '');                    return this },
+			attr:     function(k, v) { iter(i => i.setAttribute(k, v));                return this },
+			removeAttr:  function(k) { iter(i => i.removeAttribute(k));                return this },
+			parent:       function() { iter((e, i) => { sel[i] = e.parentNode });      return this },
+			remove:       function() { iter(i => i.remove());                          return this },
 
-			getAttr: function(v) { return sel[0].getAttribute(v) },
-			offset:  function(v) { return sel[0].getBoundingClientRect() },
-			sel:      function() { return sel }
+			before:       insertAdjacent('beforebegin'),
+			after:        insertAdjacent('afterend'),
+			first:        insertAdjacent('afterbegin'),
+			last:         insertAdjacent('beforeend'),
+			insertBefore: insertToAdjacent('beforebegin'),
+			insertAfter:  insertToAdjacent('afterend'),
+			insertFirst:  insertToAdjacent('afterbegin'),
+			insertLast:   insertToAdjacent('beforeend'),
+
+			prepend:      insertAdjacent('afterbegin'),
+			append:       insertAdjacent('beforeend'),
+
+			getAttr: v => sel[0].getAttribute(v),
+			offset: () => sel[0].getBoundingClientRect(),
+			sel:    () => sel,
+
+			isFemtoJS: true
 		}
 	}
+
+	$.fragment = () => $(document.createDocumentFragment())
+
+	window.$ = $
 })()
