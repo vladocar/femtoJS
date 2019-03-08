077264c29a5a20a7c40a9ec7f62ff24a752a4455
LoganDark <logandarklock@gmail.com>
2019-03-08 02:04:34 -0800

    Update femtoJS.js

--- src/femtoJS.js
+++ src/femtoJS.js
@@ -1,24 +1,41 @@
-window.$ = function(selector) {
-	let sel = [...document.querySelectorAll(selector)]
-	
-	return {
-		on:    (type, fn) => { for (let i of sel) i.addEventListener(type, fn, false);    return this },
-		css:          (v) => { for (let i of sel) i.style.cssText += v;                   return this },
-		html:         (v) => { for (let i of sel) i.innerHTML = v;                        return this },
-		insertBefore: (v) => { for (let i of sel) i.insertAdjacentHTML('beforeBegin', v); return this },
-		insertAfter:  (v) => { for (let i of sel) i.insertAdjacentHTML('afterEnd', v);    return this },
-		insertFirst:  (v) => { for (let i of sel) i.insertAdjacentHTML('afterBegin', v);  return this },
-		insertLast:   (v) => { for (let i of sel) i.insertAdjacentHTML('beforeEnd', v);   return this },
-		text:         (v) => { for (let i of sel) i.innerText = v;                        return this },
-		addClass:     (v) => { for (let i of sel) i.classList.add(v);                     return this },
-		toggleClass:  (v) => { for (let i of sel) i.classList.toggle(v);                  return this },
-		removeClass:  (v) => { for (let i of sel) i.classList.remove(v);                  return this },
-		empty:        (v) => { for (let i of sel) i.innerHTML = '';                       return this },
-		attr:      (a, v) => { for (let i of sel) i.setAttribute(a, v);                   return this },
-		getAttr:      (v) => sel[0].getAttribute(v),
-		removeAttr:   (v) => { for (let i of sel) i.removeAttribute(v);                   return this },
-		parent:       (v) => { sel.forEach((v, i) => { sel[i] = v.parentNode });          return this },
-		offset:       (v) => sel[0].getBoundingClientRect(),
-		sel:           () => sel
+(() => {
+	let argToElements = function(src) {
+		let elems = []
+
+		if (typeof src == 'string') {
+			elems = [...document.querySelectorAll(src)]
+		} else if (src instanceof HTMLElement) {
+			elems.push(src)
+		} else if (Array.isArray(src)) {
+			src.forEach(i => elems.push(...argToElements(i)))
+		}
+
+		return elems
 	}
-}
+
+	window.$ = function(src) {
+		let sel = argToElements(src)
+
+		return {
+			on:    function(type, fn) { sel.forEach(i => i.addEventListener(type, fn, false));    return this },
+			css:          function(v) { sel.forEach(i => i.style.cssText += v);                   return this },
+			html:         function(v) { sel.forEach(i => i.innerHTML = v);                        return this },
+			insertBefore: function(v) { sel.forEach(i => i.insertAdjacentHTML('beforeBegin', v)); return this },
+			insertAfter:  function(v) { sel.forEach(i => i.insertAdjacentHTML('afterEnd', v));    return this },
+			insertFirst:  function(v) { sel.forEach(i => i.insertAdjacentHTML('afterBegin', v));  return this },
+			insertLast:   function(v) { sel.forEach(i => i.insertAdjacentHTML('beforeEnd', v));   return this },
+			text:         function(v) { sel.forEach(i => i.innerText = v);                        return this },
+			addClass:     function(v) { sel.forEach(i => i.classList.add(v));                     return this },
+			toggleClass:  function(v) { sel.forEach(i => i.classList.toggle(v));                  return this },
+			removeClass:  function(v) { sel.forEach(i => i.classList.remove(v));                  return this },
+			empty:         function() { sel.forEach(i => i.innerHTML = '');                       return this },
+			attr:      function(a, v) { sel.forEach(i => i.setAttribute(a, v));                   return this },
+			removeAttr:   function(v) { sel.forEach(i => i.removeAttribute(v));                   return this },
+			parent:        function() { sel.forEach((v, i) => { sel[i] = v.parentNode });         return this },
+
+			getAttr: function(v) { return sel[0].getAttribute(v) },
+			offset:  function(v) { return sel[0].getBoundingClientRect() },
+			sel:      function() { return sel }
+		}
+	}
+})()
